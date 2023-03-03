f1e5b2d38ddcc3ed1db624d047700065ae0b557e
sooyoungkang <kang.sooyoung@hotmail.com>
2023-03-04 02:01:01 +0900

    Enable user to join room

--- src/controllers/ChatRoomController.ts
+++ src/controllers/ChatRoomController.ts
@@ -5,16 +5,18 @@ import {
     OnDisconnect,
     MessageBody,
     OnMessage,
+    SocketRequest,
+    SocketQueryParam
 } from 'socket-controllers';
 import { Socket } from 'socket.io';
 import { Service } from 'typedi';
 import { BaseController } from './BaseController';
 
-@SocketController("/room/:roomId")
+@SocketController("/room")
 @Service()
 export class ChatRoomController extends BaseController {
     @OnConnect()
-    connection(@ConnectedSocket() socket: Socket) {
+    connection(@ConnectedSocket() socket: Socket,) {
         console.log('client connected to chat room');
     }
 
@@ -25,9 +27,18 @@ export class ChatRoomController extends BaseController {
 
     @OnMessage('save')
     save(@ConnectedSocket() socket: Socket, @MessageBody() message: any) {
-        console.log('received message:', message);
-        console.log('setting id to the message and sending it back to the client');
-        message.id = 1;
+        console.log("message received");
         socket.emit('message_saved', message);
+    }
+
+
+    @OnMessage('join')
+    join(@ConnectedSocket() socket: Socket, @SocketQueryParam("roomId") roomId: number, @SocketQueryParam("name") name: string, @MessageBody() message: any) {
+
+        socket.join(`room${roomId}`)
+        console.log(`${name} joined room${roomId} `)
+        socket.emit("join_success")
+
+
     }
 }
